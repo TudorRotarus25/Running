911e1920d0c2bb976c801854ad7fe732d4581bd9
tudor.rotarus <tudor.rotarus@gmail.com>
2015-12-07 01:05:32 +0200

    Tema 2 - First Commit

--- app/src/main/java/com/devacademy/running/HomePageActivity.java
+++ app/src/main/java/com/devacademy/running/HomePageActivity.java
@@ -1,11 +1,13 @@
 package com.devacademy.running;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -13,8 +15,15 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        Button beginRun = (Button) findViewById(R.id.begin_run);
+        beginRun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePageActivity.this, MyListActivity.class);
+                startActivity(intent);
+            }
+        });
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/devacademy/running/LoginActivity.java
+++ app/src/main/java/com/devacademy/running/LoginActivity.java
@@ -18,17 +18,6 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         Button signIn = (Button) findViewById(R.id.sign_in_button);
         signIn.setOnClickListener(new View.OnClickListener(){
